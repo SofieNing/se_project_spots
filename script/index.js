068b2b13e4fd3865587db8a91433b5e1619de41f
SofieNing <jinglebellsrd@gmail.com>
2025-05-23 08:54:15 -0400

    add array objects

--- script/index.js
+++ script/index.js
@@ -1,4 +1,17 @@
-console.log("Hello,world!!");
+// const array = ("string", 3, null);
+// const array = [{objects}]; ex: [{(key)foo: (value)"bar",}];
+
+const initialCards = [{name: "Val Thorens", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg"},
+    {name: "Restaurant terrace", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg"},
+    {name: "An outdoor cafe", link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg"},
+    {name: "A very long bridge, over the forest and through the trees", 
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg"},
+    {name: "Tunnel with morning light", 
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg"},
+    {name: "Mountain house", 
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg"},
+];
+
 
 const editprofileEditBtn = document.querySelector(".profile__edit-btn");
 const editprofileModal = document.querySelector("#edit-profile-modal");
@@ -66,6 +79,12 @@ function handleaddNewPostSubmit(evt){
     closeModal(newpostModal);
 }
      addNewPostForm.addEventListener("submit",handleaddNewPostSubmit);
+
+
+     initialCards.forEach(function(item) {
+        console.log(item.name);
+        console.log(item.link);
+     });
 
 
 
